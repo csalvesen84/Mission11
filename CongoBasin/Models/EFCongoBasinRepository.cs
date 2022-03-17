fd72a5e513afdc5ba6da9337157cd34410a79ee1
csalvesen84 <73505297+csalvesen84@users.noreply.github.com>
2022-03-16 21:57:32 -0600

    com

---
+++ CongoBasin/Models/EFCongoBasinRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CongoBasin.Models
+{
+    public class EFCongoBasinRepository : ICongoBasinRepository
+    {
+
+        private BookstoreContext context { get; set; }
+        public EFCongoBasinRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+
+        public void SaveBook(Book b)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateBook(Book b)
+        {
+            context.Add(b);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Book b)
+        {
+            context.Remove(b);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ CongoBasin/Models/ICongoBasinRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CongoBasin.Models
+{
+    public interface ICongoBasinRepository
+    {
+        IQueryable<Book> Books { get; }
+
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
+    }
+}
